fb682cfe1747251feadfd99c02c14e3add9d48e6
maksp <markin.max.003@gmail.com>
2023-06-05 02:46:59 +0300

    ю

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-#include <graphics.h> 
-
-int main() {
-    int gd = DETECT, gm;
-    initgraph(&gd, &gm, "");
+#include "graphics.h"
+#pragma comment(lib,"graphics.lib")
+int main()
+{
+    char pathtodriver[] = "";
+    int driver, mode;
+    driver = DETECT; /* автоопределение */
+    mode = 0;
+    initgraph(&driver, &mode, pathtodriver);
 
     int x0 = 100, y0 = 100, size = 200;
     int x1 = x0 + size, y1 = y0;
@@ -17,11 +21,10 @@ int main() {
     line(x3, y3, x0, y0);
 
     // Заполняем квадрат заданными линиями
-    for (int i = x0; i <= x2; i += 10) {
-        line(i, y0, x2, y2 - (x2 - i));
-        line(i, y1, x3, y3 + (x2 - i));
-    }
-
+    line((x0+x1)/2, (y0+y1)/2, x3, (y0+y2)/2);
+    line((x0 + x1) / 2, (y0 + y1) / 2, x2, y2);
+    line((x0 + x1) / 2, (y0 + y1) / 2, (x3+x2)/2, (y3+y2)/2);
+    line((x0 + x1) / 2, (y0 + y1) / 2, (x3+x2)/3, y3);
     getch();
     closegraph();
     return 0;
